aaf219ea3da70c740690de5b68c158f454323304
Sarad Babu Shrestha <saradbabushrestha@Sarad.local>
2024-11-06 21:32:51 +0545

    Custom Buttom and homepage

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from "./CustomButton";
+import FilePicker from "./FilePicker";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import Tab from "./Tab";
+
+export { CustomButton, FilePicker, AIPicker, ColorPicker, Tab };
